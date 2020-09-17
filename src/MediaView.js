bc31545b717f43e91157eae22b42c035e1e06704
David <a26364177@gmail.com>
2020-09-17 22:13:41 +0800

    change media name

--- src/MediaView.js
+++ src/MediaView.js
@@ -1,28 +1,28 @@
-import React, { createRef, useEffect, useContext } from "react";
-import { findNodeHandle, requireNativeComponent } from "react-native";
-import { NativeAdContext } from "./context";
+import React, {createRef, useEffect, useContext} from 'react'
+import {findNodeHandle, requireNativeComponent} from 'react-native'
+import {NativeAdContext} from './context'
 
 const MediaView = (props) => {
-  const { nativeAd, nativeAdView } = useContext(NativeAdContext);
-  const adMediaView = createRef();
+  const {nativeAd, nativeAdView} = useContext(NativeAdContext)
+  const adMediaView = createRef()
 
   const _onLayout = () => {
-    if (!nativeAdView) return;
-    let handle = findNodeHandle(adMediaView.current);
+    if (!nativeAdView) return
+    let handle = findNodeHandle(adMediaView.current)
     nativeAdView.setNativeProps({
       mediaview: handle,
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    _onLayout();
-  }, [nativeAd, nativeAdView]);
+    _onLayout()
+  }, [nativeAd, nativeAdView])
 
   return (
     <AdMediaView ref={adMediaView} onLayout={_onLayout} style={props.style} />
-  );
-};
+  )
+}
 
-const AdMediaView = requireNativeComponent("MediaView", MediaView);
+const AdMediaView = requireNativeComponent('AdmobMediaView', MediaView)
 
-export default MediaView;
+export default MediaView
