11c88ab4ea4ffd7a22abe240dca2432c82ace801
David <a26364177@gmail.com>
2020-09-17 22:32:34 +0800

    remove star view

--- src/StarRatingView.js
+++ src/StarRatingView.js
@@ -1,30 +1,9 @@
-import React, { useContext, createRef, useEffect } from "react";
-import { findNodeHandle } from "react-native";
-import StarView from "./components/StarView";
-import { NativeAdContext } from "./context";
+import React, {useContext, createRef, useEffect} from 'react'
+import {findNodeHandle} from 'react-native'
+import {NativeAdContext} from './context'
 
 const StarRatingView = (props) => {
-  const { nativeAd, nativeAdView } = useContext(NativeAdContext);
-  const starRatingRef = createRef();
-  const _onLayout = () => {
-    if (!nativeAdView) return;
-    let handle = findNodeHandle(starRatingRef.current);
-    nativeAdView.setNativeProps({
-      starrating: handle,
-    });
-  };
-  useEffect(() => {
-    _onLayout();
-  }, [nativeAd, nativeAdView]);
+  return null
+}
 
-  return nativeAd && nativeAd.rating && nativeAd.rating > 0 ? (
-    <StarView
-      {...props}
-      ref={starRatingRef}
-      stars={nativeAd.rating ? nativeAd.rating : 0}
-      onLayout={_onLayout}
-    />
-  ) : null;
-};
-
-export default StarRatingView;
+export default StarRatingView
